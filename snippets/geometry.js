537400dc15b6eefb5290eef1b24a1d7aeea43f29
Eoghan Garvey <eoghanogairbhi@gmail.com>
2013-05-09 18:14:37 +0100

    w i p

---
+++ snippets/geometry.js
@@ -0,0 +1,17 @@
+
+
+var sphereGeo = new THREE.SphereGeometry( 000, 000, 000 );
+var cylinderGeo =new THREE.CylinderGeometry( 000, 000, 000, 000 );
+
+var xxx = new THREE.Mesh( xxx, xxx );
+
+var xxx = new THREE.Object3D();
+
+xxx.position.x = 000;
+xxx.position.y = 000;
+xxx.position.z = 000;
+
+xxx.rotation.x = 000;
+xxx.rotation.y = 000;
+xxx.rotation.z = 000;
+
---
+++ snippets/material.js
@@ -0,0 +1,2 @@
+var xxx = new THREE.MeshPhongMaterial( { color: 0x888888, specular: 0x888888, shininess: 20 } );
+
